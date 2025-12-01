c9e2199605cfae7d5ad6e8f4f1982d474242d832
Rupayan Pujari <rupayanpujari308@gmail.com>
2025-12-01 15:52:10 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import numpy as np
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load("heart_model.pkl")
+
+@app.route("/")
+def home():
+    return render_template("form.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        data = [
+            float(request.form['age']),
+            float(request.form['sex']),
+            float(request.form['cp']),
+            float(request.form['trestbps']),
+            float(request.form['chol']),
+            float(request.form['fbs']),
+            float(request.form['restecg']),
+            float(request.form['thalach']),
+            float(request.form['exang']),
+            float(request.form['oldpeak']),
+            float(request.form['slope']),
+            float(request.form['ca']),
+            float(request.form['thal'])
+        ]
+
+        prediction = model.predict([data])[0]
+
+        return render_template("result.html", result=int(prediction))
+
+    except Exception as e:
+        return f"Error: {e}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
